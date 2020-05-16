c3b81eaf9c3c891954bfa4db33eb44e08f4b6ba7
Shahein Moussavi <shaheinm@gmail.com>
2020-05-16 16:59:01 -0400

    updated test query and removed admin ui test

--- test/integration/graphql/graphql-server.spec.js
+++ test/integration/graphql/graphql-server.spec.js
@@ -4,7 +4,7 @@ describe('Strapi GraphQL endpoint', () => {
   it('should be available', (done) => {
     request('http://localhost:1337')
       .post('/graphql')
-      .send({ query: '{ articles { id } }'})
+      .send({ query: '{ practices { id } }'})
       .set('Accept', 'application/json')
       .expect(200, done);
   });
