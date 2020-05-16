e1d87411248c3ee11cf4cfb68a0f3f1e3cc385fb
Shahein Moussavi <shaheinm@gmail.com>
2020-05-16 19:31:52 -0400

    merged cicd branch to bring tests over

--- _travis/pushToQuay.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  echo "$QUAY_PASSWORD" | docker login -u "$QUAY_USERNAME" --password-stdin quay.io
-  docker push openpracticelibrary/opl-content-api
-fi
-
