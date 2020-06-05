a9f7aab02897804d672fe6ff9cfdf95f1f4e2b93
Shahein Moussavi <shaheinm@gmail.com>
2020-06-05 17:13:35 -0400

    new strapi db config

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 module.exports = ({ env }) => {
   return {
-    'defaultConnection': 'default',
-    'connections': {
-      'default': {
-        'connector': 'mongoose',
-        'settings': {
-          'host': `${env.DATABASE_HOST || '127.0.0.1'}`,
-          'port': `${env.DATABASE_PORT || 27017}`,
-          'database': `${env.DATABASE_NAME || 'strapi'}`,
-          'username': `${env.DATABASE_USERNAME || ''}`,
-          'password': `${env.DATABASE_PASSWORD || ''}`
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'mongoose',
+        settings: {
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env('DATABASE_PORT', 27017),
+          database: env('DATABASE_NAME', 'strapi'),
+          username: env('DATABASE_USERNAME', '' ),
+          password: env('DATABASE_PASSWORD', ''),
         },
-        'options': {}
+        options: {}
       }
     }
   };
